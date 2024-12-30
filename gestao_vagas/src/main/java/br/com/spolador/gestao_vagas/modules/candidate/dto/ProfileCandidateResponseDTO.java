9a6bc5cc29433006df8cf3ca6c3f5d44831b88f9
luizspolador <luizh.spolador@gmail.com>
2024-12-30 14:41:23 -0300

    retorno de erros

--- gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -1,5 +1,6 @@
 package br.com.spolador.gestao_vagas.modules.candidate.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +12,13 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProfileCandidateResponseDTO {
-
+    @Schema(example = "Desenvolvedor Java")
     private String description;
+    @Schema(example = "luizh")
     private String username;
+    @Schema(example = "luizh@gmail.com")
     private String email;
     private UUID id;
+    @Schema(example = "Luiz Henrique")
     private String name;
 }
