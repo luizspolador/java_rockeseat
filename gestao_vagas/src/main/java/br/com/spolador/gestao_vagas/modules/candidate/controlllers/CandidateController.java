cc04586bf6d964f66a7bfe77c6eeb4b1ad01468c
luizspolador <luizh.spolador@gmail.com>
2024-12-30 13:22:55 -0300

    adicionando swagger controller

--- gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/candidate/controlllers/CandidateController.java
+++ gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/candidate/controlllers/CandidateController.java
@@ -5,6 +5,13 @@ import br.com.spolador.gestao_vagas.modules.candidate.useCases.CreateCandidateUs
 import br.com.spolador.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.spolador.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
 import br.com.spolador.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,6 +59,14 @@ public class CandidateController {
 
     @GetMapping("/job")
     @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Listagem de vagas disponível para o candidato", description = "Essa função é responsável por" +
+            " listar todas as vagas disponíveis, baseada no filtro")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+            })
+    })
     public List<JobEntity> findJobByFilter(@RequestParam String filter){
         return this.listAllJobsByFilterUseCase.execute(filter);
     }
