f22920cb43036c6fb349c62d2dc56b3f87ba3e7b
luizspolador <luizh.spolador@gmail.com>
2024-12-30 13:48:26 -0300

    adicionando exemplos e swagger ao JobController

--- gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,11 +1,15 @@
 package br.com.spolador.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
 
+    @Schema(example = "Vaga para pessoa desenvolvedora Java junior", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
+    @Schema(example = "Gympass, plano de saúde", requiredMode = Schema.RequiredMode.REQUIRED)
     private String benefits;
+    @Schema(example = "Junior", requiredMode = Schema.RequiredMode.REQUIRED)
     private String level;
 }
--- gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/company/entities/JobEntity.java
+++ gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/company/entities/JobEntity.java
@@ -1,5 +1,6 @@
 package br.com.spolador.gestao_vagas.modules.company.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -20,10 +21,13 @@ public class JobEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Schema(example = "Vaga para Design")
     private String description;
+    @Schema(example = "Gympass, Plano de saúde")
     private String benefits;
 
     @NotBlank(message = "Esse campo é obrigatório")
+    @Schema(example = "Senior")
     private String level;
 
     @ManyToOne()
