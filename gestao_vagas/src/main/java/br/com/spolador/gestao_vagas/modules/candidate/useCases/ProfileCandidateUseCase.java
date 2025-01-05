e8b417ac313702d09b08e56bf24bcae5d4faf993
luizspolador <luizh.spolador@gmail.com>
2025-01-05 11:39:50 -0300

    refatorando

--- gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
+++ gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -1,9 +1,9 @@
 package br.com.spolador.gestao_vagas.modules.candidate.useCases;
 
+import br.com.spolador.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.spolador.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.spolador.gestao_vagas.modules.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -17,7 +17,7 @@ public class ProfileCandidateUseCase {
     public ProfileCandidateResponseDTO execute(UUID idCandidate){
         var candidate = this.candidateRepository.findById(idCandidate)
                 .orElseThrow(() -> {
-                    throw new UsernameNotFoundException("User not found");
+                    throw new UserNotFoundException();
                 });
         var candidateDTO = ProfileCandidateResponseDTO.builder()
                 .description(candidate.getDescription())
