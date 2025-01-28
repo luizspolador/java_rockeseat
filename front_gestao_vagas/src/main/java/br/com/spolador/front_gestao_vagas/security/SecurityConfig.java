1bdb70280996697fde7f6c38d196f452bcc4fae0
luizspolador <luizh.spolador@gmail.com>
2025-01-28 17:57:35 -0300

    inserindo o security

---
+++ front_gestao_vagas/src/main/java/br/com/spolador/front_gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.spolador.front_gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/candidate/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/candidate/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
+}
