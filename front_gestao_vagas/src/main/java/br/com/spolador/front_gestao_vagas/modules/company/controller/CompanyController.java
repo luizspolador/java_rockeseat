4331f0d33a598a215ac3bdcb74064c8e647fe35c
luizspolador <luizh.spolador@gmail.com>
2025-01-30 16:41:35 -0300

    cadastro de jobs

--- front_gestao_vagas/src/main/java/br/com/spolador/front_gestao_vagas/modules/company/controller/CompanyController.java
+++ front_gestao_vagas/src/main/java/br/com/spolador/front_gestao_vagas/modules/company/controller/CompanyController.java
@@ -1,13 +1,16 @@
 package br.com.spolador.front_gestao_vagas.modules.company.controller;
 
 import br.com.spolador.front_gestao_vagas.modules.company.dto.CreateCompanyDTO;
+import br.com.spolador.front_gestao_vagas.modules.company.dto.CreateJobsDTO;
 import br.com.spolador.front_gestao_vagas.modules.company.service.CreateCompanyService;
+import br.com.spolador.front_gestao_vagas.modules.company.service.CreateJobService;
 import br.com.spolador.front_gestao_vagas.modules.company.service.LoginCompanyService;
 import br.com.spolador.front_gestao_vagas.utils.FormatErrorMessage;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,6 +31,9 @@ public class CompanyController {
 
     @Autowired
     private LoginCompanyService loginCompanyService;
+
+    @Autowired
+    private CreateJobService createJobService;
 
     @GetMapping("/create")
     public String create(Model model){
@@ -72,8 +78,20 @@ public class CompanyController {
     }
 
     @GetMapping("/jobs")
-    @PreAuthorize("hasRoles('COMPANY')")
-    public String jobs(){
+    @PreAuthorize("hasRole('COMPANY')")
+    public String jobs(Model model){
+        model.addAttribute("jobs", new CreateJobsDTO());
         return "company/jobs";
+    }
+
+    @PostMapping("/jobs")
+    @PreAuthorize("hasRole('COMPANY')")
+    public String createJobs(CreateJobsDTO jobs){
+        var result = this.createJobService.execute(jobs, getToken());
+        return "redirect:/company/jobs";
+    }
+    private String getToken(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getDetails().toString();
     }
 }
---
+++ front_gestao_vagas/src/main/java/br/com/spolador/front_gestao_vagas/modules/company/service/CreateJobService.java
@@ -0,0 +1,27 @@
+package br.com.spolador.front_gestao_vagas.modules.company.service;
+
+import br.com.spolador.front_gestao_vagas.modules.company.dto.CreateJobsDTO;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class CreateJobService {
+
+    public String execute(CreateJobsDTO jobs, String token){
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(token);
+
+        HttpEntity<CreateJobsDTO> request = new HttpEntity<>(jobs, headers);
+
+        var result = rt.postForObject("http://localhost:8080/company/job/", request, String.class);
+
+        System.out.println(result);
+
+        return result;
+    }
+}
